6f490c3a92b3b7e7ec201e64c4c9183d6fd5052a
John P <70922864+jpierce-coder@users.noreply.github.com>
2022-08-22 17:41:49 -0500

    Complete Warm Up Exercise 08-22
    Wrote function to check if the function value (num) is an integer or not, and to log to the console a message depending on boolean given from the check.

---
+++ JavaScript/warmUp-Exercise-08-22.js
@@ -0,0 +1,9 @@
+function isInteger(num) {
+    if (Number.isInteger(num)) {
+        console.log('The number is an integer!');
+    } else {
+        console.log('The number is NOT an integer!');
+    }
+};
+
+isInteger(23.4);
