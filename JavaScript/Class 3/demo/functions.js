974574166228c0ca4a4890e71e0b41bf392a16d5
John P <70922864+jpierce-coder@users.noreply.github.com>
2022-08-22 19:25:22 -0500

    In-Class Demonstrations

---
+++ JavaScript/Class 3/demo/functions.js
@@ -0,0 +1,9 @@
+// Functions are tools
+
+// Add two numbers
+
+function sum(a,b) {
+    return a + b;
+}
+
+console.log(sum('Hi there, ', 'my name is John.'));
---
+++ JavaScript/Class 3/demo/loops.js
@@ -0,0 +1,4 @@
+// Printing numbers 0 - 9
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
